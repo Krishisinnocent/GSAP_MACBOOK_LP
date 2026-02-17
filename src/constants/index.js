9e635d54bd48512c0836030d0e57cbff1e737610
krishisinnocent <krishsheth963@gmail.com>
2026-02-17 19:35:56 +0530

    Implement her section

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ export const navLinks= [
     {label: 'Store'},
     {label: 'Mac'},
     {label: 'iPhone'},
-    {label: 'watch'},
+    {label: 'Watch'},
     {label: 'Vision'},
     {label: 'AirPods'},
 ];
